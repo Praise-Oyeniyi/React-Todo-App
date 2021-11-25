b04086a120625f6a8b9bd36c24b875d8528c17a4
Oyeniyi Praise <oyeniyimail@gmail.com>
2021-11-25 14:51:39 +0100

    Add files via upload

---
+++ build/precache-manifest.5b88cb761d73d1f510f437df167a75a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6c950905c618fa3cc5",
+    "url": "/static/css/main.6a6205b9.chunk.css"
+  },
+  {
+    "revision": "0a6c950905c618fa3cc5",
+    "url": "/static/js/main.0a6c9509.chunk.js"
+  },
+  {
+    "revision": "8ef5b289497f0d58aa36",
+    "url": "/static/js/1.8ef5b289.chunk.js"
+  },
+  {
+    "revision": "2f612ab929ba3e21ee6f",
+    "url": "/static/js/2.2f612ab9.chunk.js"
+  },
+  {
+    "revision": "9337356c7c3728a2dfc9",
+    "url": "/static/js/runtime~main.9337356c.js"
+  },
+  {
+    "revision": "b33a97ddfe6fb8f78a754d5c28c722ca",
+    "url": "/static/media/photo-1603468620905-8de7d86b781e.b33a97dd.jfif"
+  },
+  {
+    "revision": "b653961c5b0ba6f2074e1caa7d4f7f08",
+    "url": "/index.html"
+  }
+];
